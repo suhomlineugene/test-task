3748ff2c56bf99946af574d8eabfc3a4b307eb2f
Eugene Sukhomlyn <nemesises@live.com>
2023-03-26 23:17:54 +0300

    finished

--- test-task/src/app/app.component.ts
+++ test-task/src/app/app.component.ts
@@ -3,7 +3,7 @@ import { LazyLoadEvent } from 'primeng/api';
 import { Paginator } from 'primeng/paginator';
 import { Table } from 'primeng/table';
 import { finalize } from 'rxjs';
-import { TestFileServiceProxy } from 'src/shared/service-proxies';
+import { TestFileDto, TestFileServiceProxy } from 'src/shared/service-proxies';
 import { AppComponentBase } from './app-component-base';
 
 @Component({
@@ -24,9 +24,11 @@ export class AppComponent extends AppComponentBase implements OnInit {
   }
 
   filedata: any;
-
+  data: any;
+  chartsData: any;
+  chartsOptions: any;
   ngOnInit(): void {
-    //this.getTableData();
+    this.getChartsData();
   }
 
   getTableData(event?: LazyLoadEvent): void {
@@ -44,5 +46,78 @@ export class AppComponent extends AppComponentBase implements OnInit {
         this.primengTableHelper.records = result;
         this.primengTableHelper.hideLoadingIndicator();
       });
+
+    this;
+  }
+
+  getChartsData(): any {
+    const documentStyle = getComputedStyle(document.documentElement);
+    const textColor = documentStyle.getPropertyValue('--text-color');
+    const textColorSecondary = documentStyle.getPropertyValue(
+      '--text-color-secondary'
+    );
+    const surfaceBorder = documentStyle.getPropertyValue('--surface-border');
+
+    this._testFileService.getFileData().subscribe((result) => {
+      this.chartsData = {
+        labels: result.map((obj: any) => {
+          return obj.siteName;
+        }),
+        datasets: [
+          {
+            label: 'CLUSTER_MEDIAN_PRICE',
+            backgroundColor: documentStyle.getPropertyValue('--blue-500'),
+            borderColor: documentStyle.getPropertyValue('--blue-500'),
+            data: result.map((obj: any) => {
+              return obj.clusterMedianPrice;
+            }),
+          },
+          {
+            label: 'CLIENT_MARKET_PRICE',
+            backgroundColor: documentStyle.getPropertyValue('--pink-500'),
+            borderColor: documentStyle.getPropertyValue('--pink-500'),
+            data: result.map((obj: any) => {
+              return obj.clientMarketPrice;
+            }),
+          },
+        ],
+      };
+    });
+
+    this.chartsOptions = {
+      indexAxis: 'y',
+      maintainAspectRatio: false,
+      aspectRatio: 0.8,
+      plugins: {
+        legend: {
+          labels: {
+            color: textColor,
+          },
+        },
+      },
+      scales: {
+        x: {
+          ticks: {
+            color: textColorSecondary,
+            font: {
+              weight: 500,
+            },
+          },
+          grid: {
+            color: surfaceBorder,
+            drawBorder: false,
+          },
+        },
+        y: {
+          ticks: {
+            color: textColorSecondary,
+          },
+          grid: {
+            color: surfaceBorder,
+            drawBorder: false,
+          },
+        },
+      },
+    };
   }
 }
